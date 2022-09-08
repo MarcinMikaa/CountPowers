d0a07e8c0aa6e69229bef58fdc67c52d1b6c1b3f
uczen <uczen@uczen.ckziu.net>
2022-09-08 10:49:59 +0200

    Comments added

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+# Zadaniem programu jest znalezienie ile w sumie różnych potęg dwójek trzeba do siebie dodać,
+# żeby otrzymać szukaną liczbę.
+
 class CountPowers:
     def __init__(self, n):
         self.n = n
 
+    # Znajdujemy i zwracamy największą potęgę liczby 2, która jest mniejsza od przekazanej liczby.
     def __find(self):
         power = 1
         while power * 2 <= self.n:
             power *= 2
         return power
 
+    # Sprawdzamy czy szukana liczba jest różna od zera, jeśli tak, to odejmujemy od niej liczbę zwróconą
+    # przez wywołaną funkcję __find(), a następnie zwiększamy counter o jeden. Jeżeli szukana liczba jest równa 0
+    # to zwracamy counter, który przechowuje ilość potęg 2, które trzeba do siebie dodać aby uzyskać szukaną liczbę.
     def count(self):
         counter = 0
         while self.n != 0:
@@ -16,5 +23,5 @@ class CountPowers:
         return counter
 
 
-c1 = CountPowers(18)
+c1 = CountPowers(15)
 print(c1.count())
